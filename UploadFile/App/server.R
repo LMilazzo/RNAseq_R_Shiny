51a9d920d614e41ad1cd751d84dcf23f3974b31f
lucas milazzo <137016516+LMilazzo@users.noreply.github.com>
2024-07-03 13:24:59 -0400

    Update server.R comment format

--- UploadFile/App/server.R
+++ UploadFile/App/server.R
@@ -1,20 +1,23 @@
-#Server and functions to build and rebuild the output of the applet
-
-
-#_________________________Shiny Server__________________________________________
-
+#_________________________________Shiny Server__________________________________________
 server <- function(input, output) {
   
-#############################Observables########################################
- 
-  #Observe UI event [ merged_gene_counts_uploaded_file ]
-  #Creates raw_counts
-  #Creates gene_names
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~######_______________Observables________________######~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+  #-----Observe UI Event------# ----> merged_gene_counts_uploaded_file
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~#
   observeEvent(input$merged_gene_counts_uploaded_file,
     {
-    #Upon submission of UI object [ merged_gene_counts_uploaded_file ]      
+      #Upon submission of UI object [ merged_gene_counts_uploaded_file ]      
       
-      #Expect c(raw_counts, gene_names)           
+      #Goal:
+      #             |----->> gene_names
+      #   input ----|
+      #             |----->> raw_counts
+
+      #Expect as return c(raw_counts, gene_names)           
       func_return <- Set_Clean_Counts(input$merged_gene_counts_uploaded_file$datapath)
       
       raw_counts <<- data.frame(func_return[1])
@@ -25,15 +28,19 @@ server <- function(input, output) {
   )
   
  
-###########################Output Objects#######################################
-  
-  #output$raw_counts_PreviewTable
-    #A Data Table preview of what is inside the uploaded counts table 
-    #after formating correctly
-      #Expected Format:
-      #axis.names| Sample1 | Sample 2 | Sample 3
-      #Gene_ID_1 |    x    |    x     |    x
-      #Gene_ID_2 |    x    |    x     |    x
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~######_____________Output$ Objects______________######~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+  #--------raw_counts_PreviewTable---------#
+  #~~~~~~~~~~~~~~Data Table~~~~~~~~~~~~~~~~#
+  #A Data Table preview of what is inside the uploaded counts table 
+  #after formating correctly
+    #Expected Format:
+    #axis.names| Sample1 | Sample 2 | Sample 3
+    #Gene_ID_1 |    x    |    x     |    x
+    #Gene_ID_2 |    x    |    x     |    x
   output$raw_counts_PreviewTable <- renderDT(
     {
     req(input$merged_gene_counts_uploaded_file)
@@ -44,13 +51,15 @@ server <- function(input, output) {
     
     }, rownames = TRUE
   )
-  
-  #output$geneID_geneName_PreviewTable
-    #A Data Table preview of all gene ids and their gene names
-      #Expected Format:
-      #axis.names|    gene    | gene_name
-      #   1      | Gene_ID_1  |   Actin    
-      #   2      | Gene_ID_2  |   COOR4_7    
+
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+  #------geneID_geneName_PreviewTable------#
+  #~~~~~~~~~~~~~~Data Table~~~~~~~~~~~~~~~~#
+  #A Data Table preview of all gene ids and their gene names
+    #Expected Format:
+    #axis.names|    gene    | gene_name
+    #   1      | Gene_ID_1  |   Actin    
+    #   2      | Gene_ID_2  |   COOR4_7    
   output$geneID_geneName_PreviewTable <- renderDT(
     {
       req(input$merged_gene_counts_uploaded_file)
@@ -67,5 +76,5 @@ server <- function(input, output) {
   
   
   
-}  #End of App_________________________________________________________________
+}#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X#X
 
